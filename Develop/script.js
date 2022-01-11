dd70d01c5b5445679124bc3e3c73b2b7e78cc7eb
carolpang <carolpang17@gmail.com>
2022-01-11 12:41:06 -0800

    set Password length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -21,3 +21,27 @@ function generatePassword() {
   var password = '';
 }
 
+//function to set Password length
+var  getPasswordLength = function() {
+  var pwLength = "";
+
+  while (pwLength === "" || pwLength === null) {
+    pwLength = prompt("Please choose a length of at least 8 characters and no more than 128 characters");
+
+    if (pwLength < 8 ){
+      window.alert("Please choose a length of at least 8 characters.");
+      pwLength = "";
+    }
+
+    if (pwLength > 128 ){
+      window.alert("Please choose a length that no more than 128 characters.");
+      pwLength = "";
+    }
+  }
+  //console.log("Your Password length is " + pwLength);
+
+
+  return pwLength;
+};
+
+getPasswordLength();
